9e78f96c572ed085378641db867231a5046b1531
Rayane Paiva <rayanepaiiva@live.com>
2022-05-06 15:41:11 -0300

    model ModeloEquipamento criado

---
+++ src/main/java/model/ModeloEquipamento.java
@@ -0,0 +1,23 @@
+package model;
+
+public class ModeloEquipamento {
+    private String uuid;
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    private String nome;
+}
