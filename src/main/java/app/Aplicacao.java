91cbd7eb17b1a64e3d35991d87619753fed226f4
Rayane Paiva <rayanepaiiva@live.com>
2022-05-08 19:49:28 -0300

    endpoints Estado Equipamento

--- src/main/java/app/Aplicacao.java
+++ src/main/java/app/Aplicacao.java
@@ -2,11 +2,14 @@ package app;
 
 import org.json.JSONObject;
 import service.EquipamentoService;
+import service.EstadoEquipamentoService;
+
 import static spark.Spark.*;
 
 public class Aplicacao {
 
     private static EquipamentoService equipamentoService = new EquipamentoService();
+    private static EstadoEquipamentoService estadoEquipamentoService = new EstadoEquipamentoService();
 
     public static void main(String[] args) {
 
@@ -17,7 +20,13 @@ public class Aplicacao {
         get ("/equipamento/delete/:id", (request, response) -> equipamentoService.delete(request, response));
         get("/equipamentos", (request, response) -> equipamentoService.getAll(request, response));
 
-        post("/modeloequipamento", (request, response) -> equipamentoService.add(request, response));
+
+        post("/estado/equipamento", (request, response) -> estadoEquipamentoService.add(request, response));
+        get("/estado/equipamento/read/:id", (request, response) -> estadoEquipamentoService.get(request, response));
+        put("/estado/equipamento/update/:id", (request, response) -> estadoEquipamentoService.update(request, response));
+        get("/estado/equipamento/delete/:id", (request, response) -> estadoEquipamentoService.delete(request, response));
+        get("/estado/equipamentos", (request, response) -> estadoEquipamentoService.getAll(request, response));
+
 
 
 
