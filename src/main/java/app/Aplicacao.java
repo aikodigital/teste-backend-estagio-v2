2aae2ebc591046fe53e2c7c4ed33d0c1b17e4e1d
Rayane Paiva <rayanepaiiva@live.com>
2022-05-08 01:00:27 -0300

    endpoints Equipamento

--- src/main/java/app/Aplicacao.java
+++ src/main/java/app/Aplicacao.java
@@ -1,2 +1,25 @@
-package app;public class Aplicacao {
+package app;
+
+import org.json.JSONObject;
+import service.EquipamentoService;
+import static spark.Spark.*;
+
+public class Aplicacao {
+
+    private static EquipamentoService equipamentoService = new EquipamentoService();
+
+    public static void main(String[] args) {
+
+
+        post("/equipamento", (request, response) -> equipamentoService.add(request, response));
+        get("/equipamento/read/:id", (request, response) -> equipamentoService.get(request, response));
+        put("/equipamento/update/:id", (request, response) -> equipamentoService.update(request, response));
+        get ("/equipamento/delete/:id", (request, response) -> equipamentoService.delete(request, response));
+        get("/equipamentos", (request, response) -> equipamentoService.getAll(request, response));
+
+        post("/modeloequipamento", (request, response) -> equipamentoService.add(request, response));
+
+
+
+    }
 }
