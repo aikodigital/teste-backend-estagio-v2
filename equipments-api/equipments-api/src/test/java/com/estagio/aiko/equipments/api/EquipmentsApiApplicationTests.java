d6b892f02c0773e8525c5237cc9e1c2ea334f206
Mycaell de Oliveira Carneiro <oliveiramycaell@gmail.com>
2022-06-23 21:37:13 -0300

    initial commit

---
+++ equipments-api/equipments-api/src/test/java/com/estagio/aiko/equipments/api/EquipmentsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estagio.aiko.equipments.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipmentsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
