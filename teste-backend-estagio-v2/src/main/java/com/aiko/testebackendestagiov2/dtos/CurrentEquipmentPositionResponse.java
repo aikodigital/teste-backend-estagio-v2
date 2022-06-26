876882c4871f982f073b8b8af35c3df010bb9548
rosanper <rp.santana117@gmail.com>
2022-06-26 19:26:53 -0300

    feat:creating endpoint Current Equipment Position

---
+++ teste-backend-estagio-v2/src/main/java/com/aiko/testebackendestagiov2/dtos/CurrentEquipmentPositionResponse.java
@@ -0,0 +1,13 @@
+package com.aiko.testebackendestagiov2.dtos;
+
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class CurrentEquipmentPositionResponse {
+    private UUID equipmentId;
+    private String equipmentName;
+    private Float lat;
+    private Float lon;
+}
